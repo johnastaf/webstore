aff403276e8ae262e44adfac80fddddf1e31af86
John Astafev <johnastaf@gmail.com>
2018-10-31 21:11:09 +0300

    Cart amount

--- WebStore/App/header/header.tsx
+++ WebStore/App/header/header.tsx
@@ -1,8 +1,18 @@
 ﻿import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { IPhoneInCart, IStoreState } from "../store/configureStore";
+import { connect } from 'react-redux';
 
-export default class Header extends React.Component {
+interface MyProps {
+    cart: IPhoneInCart[];
+}
+
+class Header extends React.Component<MyProps, {}> {
     render() {
+        const reducer = (accumulator: number, currentValue: IPhoneInCart) => accumulator + currentValue.quantity;
+
+        let countPhonesInCart: number = this.props.cart.reduce(reducer, 0);
+
         return (
             <header>
                 <menu>
@@ -11,11 +21,19 @@ export default class Header extends React.Component {
                             <Link to="/">Catalog</Link>
                         </li>
                         <li>
-                            <Link to="/cart">Cart</Link>
+                            <Link to="/cart">Cart ({countPhonesInCart})</Link>
                         </li>
                     </ul>
                 </menu>
             </header>
         );
     }
-};
+}
+
+let mapProps = (state: IStoreState) => {
+    return {
+        cart: state.cart.cart
+    }
+}
+
+export default connect(mapProps, {})(Header) 
